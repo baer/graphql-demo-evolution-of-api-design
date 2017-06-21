1a148bb05d4b407c7394f26915281a813bb07bbb
Eric Baer <me@ericbaer.com>
2017-06-21 11:30:39 +0200

    JSONify the demo data

--- data.js
+++ data.js
@@ -1,48 +1,48 @@
 const posts = {
   "234": {
-    id: 234,
-    author: "author/9",
-    categories: ["Software Engineering"],
-    publishDate: "2016/03/27 14:00",
-    summary: "...",
-    tags: ["GraphQl", "API"],
-    title: "Contemporary API Design"
+    "id": 234,
+    "author": "author/9",
+    "categories": ["Software Engineering"],
+    "publishDate": "2016/03/27 14:00",
+    "summary": "...",
+    "tags": ["GraphQl", "API"],
+    "title": "Contemporary API Design"
   },
   "456": {
-    id: 456,
-    author: "author/4",
-    categories: ["Software Engineering"],
-    publishDate: "2016/03/27 14:00",
-    summary: "...",
-    tags: ["Redux", "redux-little-router"],
-    title: "Let The URL Do The Talking"
+    "id": 456,
+    "author": "author/4",
+    "categories": ["Software Engineering"],
+    "publishDate": "2016/03/27 14:00",
+    "summary": "...",
+    "tags": ["Redux", "redux-little-router"],
+    "title": "Let The URL Do The Talking"
   },
   "17": {
-    id: 17,
-    author: "author/7",
-    categories: ["Software Engineering"],
-    publishDate: "2016/03/27 14:00",
-    summary: "...",
-    tags: ["HTTP/2", "Interlock"],
-    title: "HTTP/2 Server Push"
+    "id": 17,
+    "author": "author/7",
+    "categories": ["Software Engineering"],
+    "publishDate": "2016/03/27 14:00",
+    "summary": "...",
+    "tags": ["HTTP/2", "Interlock"],
+    "title": "HTTP/2 Server Push"
   }
 };
 
 const authors = {
   "9": {
-    id: 9,
-    name: "Eric Baer",
-    company: "Formidable"
+    "id": 9,
+    "name": "Eric Baer",
+    "company": "Formidable"
   },
   "4": {
-    id: 4,
-    name: "Tyler Thompson",
-    company: "Formidable"
+    "id": 4,
+    "name": "Tyler Thompson",
+    "company": "Formidable"
   },
   "7": {
-    id: 7,
-    name: "Dale Bustad",
-    company: "Formidable"
+    "id": 7,
+    "name": "Dale Bustad",
+    "company": "Formidable"
   }
 };
 
