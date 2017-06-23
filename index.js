f61baf4835d8fef6c75bf7ead96cb08ff9a15226
Eric Baer <me@ericbaer.com>
2017-06-23 10:30:59 +0200

    Update data and add cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+var cors = require('cors')
 const graphqlHttp = require("express-graphql");
 const schema = require("./schema.js");
 
 const app = require("express")();
 const PORT = 5000;
+
+app.use(cors({ origin: '*' }));
 
 app.use(graphqlHttp({
   schema,
